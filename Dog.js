6e355453c2c939d0493b4a11c4499f796104c2d0
Kevin Emile <96067818+kevmill29@users.noreply.github.com>
2023-02-03 00:02:22 -0500

    Add files via upload

--- Dog.js
+++ Dog.js
@@ -10,6 +10,8 @@ class Dog {
     getDogHTML(){
         const {name, age, avatar, bio} = this
         return `
+        <img src="./images/badge-nope.png" class="badge nope hidden">
+                    <img src="./images/badge-like.png"  class="badge like hidden">
         <div class="dog-info">
                         <h1 >${name}, ${age}</h1>
                         <div class="bio">
@@ -18,7 +20,8 @@ class Dog {
                     </div>
                         <div class="dog-img-div">
                             <img class="dog-img" src="${avatar}"/>
-                        </div>`
+                        </div>
+                        `
     }
     
 }
--- index.js
+++ index.js
@@ -5,8 +5,20 @@ import dogs from './data.js'
 
 const rex = new Dog(dogs.Rex)
 
+document.getElementById('like').addEventListener('click', function(){
+    console.log('button clicked')
+    setTimeout(()=>document.querySelector('.like').classList.remove('hidden'),400
+    )
+    setTimeout(()=>document.querySelector('.like').classList.add('hidden'),1000
+    )
+    
+})
+
+
+
 function render(){
     document.getElementById('main').innerHTML = rex.getDogHTML()
     }
+
 render()
 
